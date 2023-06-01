6ad5e47ec5e100db840b636ef1ed700ee16f7839
Hezekiah Oyemade <oyedey@gmail.com>
2023-06-01 23:47:43 +0100

    virtual environment setup

---
+++ Cassie Ademola of My Professional Agenda_files/es(1).js
@@ -0,0 +1,1 @@
+localStorage.setItem("5718FABB002E","AAwcKjd96QK1hi1pPfJRuYNGNZxWfbOgfKdE3_E09mKGmm3t8mSBjayy3746TnzyYd9ehSiUC4DW6j5jTqrfM_JFKHtAbA");
---
+++ Cassie Ademola of My Professional Agenda_files/es.js
@@ -0,0 +1,1 @@
+localStorage.setItem("5718FABB002E","AAxxcQwgwE8KHRnpEO9Ug6up_rNzmJBVnhkpfD0cu_9AltwUvlFn1jPlJjrnhWCopGF20hqMFm6i3xv1sozQu_1__mus0A");
