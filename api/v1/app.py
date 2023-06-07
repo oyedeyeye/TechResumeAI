e23a02748ee2fe3787b84058339241fcd183367e
Hezekiah Oyemade <oyedey@gmail.com>
2023-06-07 12:58:49 +0100

    sysPATH correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
+
 """ Flask Application script"""
 from flask import Flask
 from models.engine.db_storage import db, DBStorage
 from models.user import User
 from models.resume_info import Resume_info
 from models.template import Template
+
+# Load environment variable
+from dotenv import load_dotenv
+load_dotenv()
 
 
 """create the app"""
