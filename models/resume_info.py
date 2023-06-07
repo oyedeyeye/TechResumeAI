1df9ce01b5ffb99703cc7de73bf794f3619f27ed
Hezekiah Oyemade <oyedey@gmail.com>
2023-06-07 13:44:05 +0100

    model correction

--- models/resume_info.py
+++ models/resume_info.py
@@ -8,11 +8,11 @@ from models.engine.db_storage import db
 class Resume_info(db.Model):
     """defines the resume information class"""
     __tablename__ = 'resume_info'
-    id = db.Model.Column(db.Integer, primary_key=True)
-    user_id = db.Model.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    template_id = db.Model.Column(db.Integer, db.ForeignKey('template.id'), nullable=False)
-    content = db.Model.Column(db.JSON, nullable=False)
-    created_at = db.Model.Column(db.DateTime, default=db.func.current_timestamp())
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    template_id = db.Column(db.Integer, db.ForeignKey('template.id'), nullable=False)
+    content = db.Column(db.JSON, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
 
     """Relationships to resume information table"""
     user = db.relationship('User', back_populates='resume_infos')
