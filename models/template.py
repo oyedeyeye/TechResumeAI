b094b023197bb81459d32b4460ccbdc8f1b73e1a
Hezekiah Oyemade <oyedey@gmail.com>
2023-06-08 17:12:48 +0100

    db update

--- models/template.py
+++ models/template.py
@@ -9,7 +9,7 @@ class Template(db.Model):
     __tablename__ = 'template'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
-    description = db.Column(db.String(256), nullable=True)
+    description = db.Column(db.String(255), nullable=True)
     layout = db.Column(db.String(128), nullable=False)
 
     """Relationships definition template table"""
